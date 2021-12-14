fc4f59ebb018ce5f8c0cf8b6fc7cee34f7922c4d
dtavana <datavana@gmail.com>
2021-12-14 15:15:16 -0500

    Add router to host forwarding

--- Project3/project3.cpp
+++ Project3/project3.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #include <map>
 #include <vector>
 #include <bitset>
+#include <fstream>
  
 #define ETHERNET_HEADER_SIZE 14
 #define IP_HEADER_SIZE 20
@@ -15,7 +16,6 @@ using namespace std;
  
 #define MAX_RECEIVE_BUFFER_SIZE 1000
 #define MAX_LOG_ENTRY_SIZE 500
-#define MAX_QUEUE_LENGTH 2
 #define ROUTER_DELAY 2000
  
 enum status {
@@ -44,11 +44,26 @@ struct TrieNode {
     struct TrieNode* zeroChild;
     struct TrieNode* oneChild;
 };
+struct NodeData {
+    int nodeId;
+    uint32_t realNetworkIp;
+    uint32_t overlayIp;
+    bool isRouter;
+};
  
 int sock;
 int currentId = 0;
-map<string, vector<QueueNode*>> outputQueues;
+int queueLength;
+int defaultTtlValue;
+NodeData* myConfig;
+NodeData* outRouter;
+
+map<uint32_t, vector<QueueNode*>> outputQueues;
 TrieNode* root;
+
+map<int, NodeData*> nodeData;
+map<int, int> outRouterData;
+map<uint32_t, map<uint32_t, int>> delayData;
 
 unsigned long stringIpToByte(string ip) {
     struct sockaddr_in to;
@@ -75,7 +90,6 @@ void addNodeToTrie(string prefix, string nextHop) {
     bitset<8> binThirdSegment(thirdSegment);
     bitset<8> binFourthSegment(fourthSegment);
     bitset<32> totalBin(binFourthSegment.to_string() + binThirdSegment.to_string() + binSecondSegment.to_string() + binFirstSegment.to_string());
-    cout << totalBin.to_string() << endl;
     TrieNode* currentNode = root;
     for(int i = 0; i < min(prefixLength, 32); ++i) {
         int currentBit = totalBin[i];
@@ -87,7 +101,6 @@ void addNodeToTrie(string prefix, string nextHop) {
                 currentNode->zeroChild->zeroChild = NULL;
                 currentNode->zeroChild->oneChild = NULL;
                 currentNode = currentNode->zeroChild;
-                cout << "Adding zero child" << endl;
                 continue; 
             } else {
                 currentNode = currentNode->zeroChild;
@@ -101,7 +114,6 @@ void addNodeToTrie(string prefix, string nextHop) {
                 currentNode->oneChild->zeroChild = NULL;
                 currentNode->oneChild->oneChild = NULL;
                 currentNode = currentNode->oneChild;
-                cout << "Adding one child" << endl;
                 continue; 
             } else {
                 currentNode = currentNode->oneChild;
@@ -114,7 +126,6 @@ void addNodeToTrie(string prefix, string nextHop) {
 }
 
 uint32_t searchForNodeInTrie(string ip) {
-    cout << "Searching for " << ip << endl;
     int firstSegment = atoi(strtok((char*) ip.c_str(), "."));
     int secondSegment = atoi(strtok(NULL, "."));
     int thirdSegment = atoi(strtok(NULL, "."));
@@ -124,33 +135,25 @@ uint32_t searchForNodeInTrie(string ip) {
     bitset<8> binThirdSegment(thirdSegment);
     bitset<8> binFourthSegment(fourthSegment);
     bitset<32> totalBin(binFourthSegment.to_string() + binThirdSegment.to_string() + binSecondSegment.to_string() + binFirstSegment.to_string());
-    cout << totalBin.to_string() << endl;
     
     TrieNode* currentNode = root;
     uint32_t lastIpSeen;
     for(int i = 0; i < 32; ++i) {
         int currentBit = totalBin[i];
-        cout << i << endl;
-        cout << currentBit << endl;
         if(currentNode->isSet) {
             lastIpSeen = currentNode->nextHopIP;
-            cout << "Setting lastIpSeen to " << byteIpToString(&lastIpSeen) << endl;
         }
         if(currentBit == 0) {
             if(currentNode->zeroChild == NULL) {
-                cout << "hello1" << endl;
                 return lastIpSeen;
             } else {
-                cout << 0 << endl;
                 currentNode = currentNode->zeroChild;
                 continue; 
             }
         } else {
             if(currentNode->oneChild == NULL) {
-                cout << "hello2" << endl;
                 return lastIpSeen;
             } else {
-                cout << 1 << endl;
                 currentNode = currentNode->oneChild;
                 continue; 
             }
@@ -185,8 +188,7 @@ void createOverlayHeaders(char* buffer, char* data, string destAddress) {
     struct udphdr* udpHeader = (struct udphdr*) (buffer + IP_HEADER_SIZE);
     char* payload = (buffer + IP_HEADER_SIZE + UDP_HEADER_SIZE);
     strncpy(payload, data, strlen(data));
-    //ipHeader->saddr = ?;
-    ipHeader->saddr = stringIpToByte("10.0.2.15");
+    ipHeader->saddr = myConfig->overlayIp;
     ipHeader->daddr = stringIpToByte(destAddress);
     ipHeader->tos = 0;
     ipHeader->frag_off = 0;
@@ -194,8 +196,7 @@ void createOverlayHeaders(char* buffer, char* data, string destAddress) {
     //ipHeader->tot_len = ?;
     ipHeader->ihl = 5;
     ipHeader->protocol = IPPROTO_UDP; // 17
-    //ipHeader->ttl = ?;
-    ipHeader->ttl = 2;
+    ipHeader->ttl = defaultTtlValue;
     ipHeader->id = currentId;
  
     udpHeader->uh_sport = MYPORT;
@@ -228,9 +229,15 @@ void sendInitialData(char* payload, uint32_t payloadSize, string destAddr) {
     int bufferSize = getBufferSize(payload);
     char buffer[bufferSize];
     createOverlayHeaders(buffer, payload, destAddr);
-    cs3516_send(sock, &payloadSize, sizeof(uint32_t), stringIpToByte("10.0.2.16"));
-    cs3516_send(sock, buffer, bufferSize, stringIpToByte("10.0.2.16"));
+    cs3516_send(sock, &payloadSize, sizeof(uint32_t), outRouter->realNetworkIp);
+    cs3516_send(sock, buffer, bufferSize, outRouter->realNetworkIp);
     currentId++;
+}
+
+int getDelay(char* buffer) {
+    struct iphdr* ipHeader = (struct iphdr*) buffer;
+    map<uint32_t, map<uint32_t, int>>::iterator allDelayMappings = delayData.find(myConfig->realNetworkIp);
+    return allDelayMappings->second.find(ipHeader->daddr)->second;
 }
  
 void sendData() {
@@ -239,7 +246,9 @@ void sendData() {
         vector<QueueNode*> newQueue;
         for(auto const queueEl : q) {
             time_t currentTimestamp = time(NULL) * 1000;
-            if(queueEl->timestamp + ROUTER_DELAY < currentTimestamp) {
+            char* buffer = *queueEl->buffer;
+            int delay = getDelay(buffer);
+            if(queueEl->timestamp + delay < currentTimestamp) {
                 char* buffer = *queueEl->buffer;
                 cs3516_send(sock, &queueEl->fileSize, sizeof(uint32_t), stringIpToByte(queueEl->destAddress));
                 cs3516_send(sock, buffer, IP_HEADER_SIZE + UDP_HEADER_SIZE + queueEl->fileSize, stringIpToByte(queueEl->destAddress));
@@ -250,18 +259,43 @@ void sendData() {
         outputQueues[el.first].swap(newQueue);
     }
 }
+
+uint32_t realIpFromOverlayIp(uint32_t overlayIp) {
+    for(auto const el : nodeData) {
+        if(el.second->overlayIp == overlayIp) {
+            return el.second->realNetworkIp;
+        }
+    }
+    return 0;
+}
  
 void enqueueData(char** buffer, uint32_t fileSize) {
     struct iphdr* ipHeader = (struct iphdr*) *buffer;
+    uint32_t realNetworkIp = realIpFromOverlayIp(ipHeader->daddr);
+    map<uint32_t, vector<QueueNode*>>::iterator directIt = outputQueues.find(realNetworkIp);
+    if(directIt != outputQueues.end()) {
+        // Direct connection to output, forward it
+        vector<QueueNode*> queue = directIt->second;
+        QueueNode* node = (QueueNode*) malloc(sizeof(QueueNode));
+        node->buffer = buffer;
+        node->fileSize = fileSize;
+        node->timestamp = time(NULL) * 1000;
+        string stringIp = byteIpToString(&realNetworkIp);
+        node->destAddress = stringIp;
+        queue.push_back(node);
+        outputQueues[realNetworkIp].swap(queue);
+        addRouterLogEntry(byteIpToString(&ipHeader->saddr), byteIpToString(&ipHeader->daddr), ipHeader->id, SENT_OKAY, byteIpToString(&ipHeader->daddr));
+        return;
+    }
     uint32_t byteIp = searchForNodeInTrie(byteIpToString(&ipHeader->daddr));
     string stringIp = byteIpToString(&byteIp);
-    map<string, vector<QueueNode*>>::iterator it = outputQueues.find(stringIp);
+    map<uint32_t, vector<QueueNode*>>::iterator it = outputQueues.find(byteIp);
     if(it == outputQueues.end()) {
         addRouterLogEntry(byteIpToString(&ipHeader->saddr), byteIpToString(&ipHeader->daddr), ipHeader->id, NO_ROUTE_TO_HOST);
         return;
     }
     vector<QueueNode*> queue = it->second;
-    if(queue.size() >= MAX_QUEUE_LENGTH) {
+    if(queue.size() >= queueLength) {
         // Drop-Tail Queueing
         addRouterLogEntry(byteIpToString(&ipHeader->saddr), byteIpToString(&ipHeader->daddr), ipHeader->id, MAX_SENDQ_EXCEEDED);
         return;
@@ -272,7 +306,7 @@ void enqueueData(char** buffer, uint32_t fileSize) {
     node->timestamp = time(NULL) * 1000;
     node->destAddress = stringIp;
     queue.push_back(node);
-    outputQueues[stringIp].swap(queue);
+    outputQueues[byteIp].swap(queue);
     addRouterLogEntry(byteIpToString(&ipHeader->saddr), byteIpToString(&ipHeader->daddr), ipHeader->id, SENT_OKAY, byteIpToString(&ipHeader->daddr));
 }
  
@@ -323,32 +357,123 @@ void recvNonBlocking(int isRouter) {
         sendData();
     }
 }
+
+void insertDelayData(int firstNodeId, int firstNodeDelay, int secondNodeId, int secondNodeDelay) {
+    map<uint32_t, map<uint32_t, int>>::iterator delayMapping;
+    map<int, NodeData*>::iterator firstNodeDataIt = nodeData.find(firstNodeId);
+    map<int, NodeData*>::iterator secondNodeDataIt = nodeData.find(secondNodeId);
+    delayMapping = delayData.find(firstNodeDataIt->second->realNetworkIp);
+    if(delayMapping == delayData.end()) {
+        map<uint32_t, int> newMap;
+        newMap.insert(make_pair(secondNodeDataIt->second->realNetworkIp, firstNodeDelay));
+        delayData.insert(make_pair(firstNodeDataIt->second->realNetworkIp, newMap));
+    } else {
+        delayData[firstNodeDataIt->second->realNetworkIp].insert(make_pair(secondNodeDataIt->second->realNetworkIp, firstNodeDelay));
+    }
+    delayMapping = delayData.find(secondNodeDataIt->second->realNetworkIp);
+    if(delayMapping == delayData.end()) {
+        map<uint32_t, int> newMap;
+        newMap.insert(make_pair(firstNodeDataIt->second->realNetworkIp, secondNodeDelay));
+        delayData.insert(make_pair(secondNodeDataIt->second->realNetworkIp, newMap));
+    } else {
+        delayData[secondNodeDataIt->second->realNetworkIp].insert(make_pair(firstNodeDataIt->second->realNetworkIp, secondNodeDelay));
+    }
+}
+
+void readConfigFile(string filename) {
+    ifstream fp(filename);
+    if(!fp.is_open()) {
+        cerr << "Could not open " << filename << endl;
+        close(sock);
+        exit(1);
+    }
+
+    string line;
+    int nodeId;
+    string realNetworkIp;
+    string overlayIp;
+    NodeData* data;
+    int firstNodeId;
+    int firstNodeDelay;
+    int secondNodeId;
+    int secondNodeDelay;
+    string prefix;
+
+    while(getline(fp, line)) {
+        int lineType = atoi(strtok((char*) line.c_str(), " "));
+        switch (lineType) {
+            case 0:
+                queueLength = atoi(strtok(NULL, " "));
+                defaultTtlValue = atoi(strtok(NULL, " "));
+                break;
+            case 1:
+                nodeId = atoi(strtok(NULL, " "));
+                realNetworkIp = strtok(NULL, " ");
+                data = (NodeData*) malloc(sizeof(NodeData));
+                data->nodeId = nodeId;
+                data->realNetworkIp = stringIpToByte(realNetworkIp);
+                data->isRouter = true;
+                nodeData.insert(make_pair(nodeId, data));
+                break;
+            case 2:
+                nodeId = atoi(strtok(NULL, " "));
+                realNetworkIp = strtok(NULL, " ");
+                overlayIp = strtok(NULL, " ");
+                data = (NodeData*) malloc(sizeof(NodeData));
+                data->nodeId = nodeId;
+                data->realNetworkIp = stringIpToByte(realNetworkIp);
+                data->overlayIp = stringIpToByte(overlayIp);
+                data->isRouter = false;
+                nodeData.insert(make_pair(nodeId, data));
+                break;
+            case 3:
+                firstNodeId = atoi(strtok(NULL, " "));
+                firstNodeDelay = atoi(strtok(NULL, " "));
+                secondNodeId = atoi(strtok(NULL, " "));
+                secondNodeDelay = atoi(strtok(NULL, " "));
+                insertDelayData(firstNodeId, firstNodeDelay, secondNodeId, secondNodeDelay);
+                break;
+            case 4:
+                firstNodeId = atoi(strtok(NULL, " "));
+                firstNodeDelay = atoi(strtok(NULL, " "));
+                prefix = strtok(NULL, " ");
+                secondNodeId = atoi(strtok(NULL, " "));
+                secondNodeDelay = atoi(strtok(NULL, " "));
+                addNodeToTrie(prefix, byteIpToString(&nodeData.find(firstNodeId)->second->realNetworkIp));
+                insertDelayData(firstNodeId, firstNodeDelay, secondNodeId, secondNodeDelay);
+                outRouterData.insert(make_pair(secondNodeId, firstNodeId));
+                break;
+        }
+    }
+    fp.close();
+}
+
+void setupOutputQueues() {
+    map<uint32_t, map<uint32_t, int>>::iterator allDelayMappings = delayData.find(myConfig->realNetworkIp);
+    for(auto const el : allDelayMappings->second) {
+        vector<QueueNode*> v;
+        outputQueues.insert(make_pair(el.first, v));
+    }
+}
  
 void runRouter() {
     cout << "I am a router" << endl;
-    sock = create_cs3516_socket(stringIpToByte("10.0.2.16"));
-    vector<QueueNode*> v;
-    outputQueues.insert(make_pair("10.0.2.17", v));
-    root = (TrieNode*) malloc(sizeof(TrieNode));
-    root->isSet = 0;
-    root->nextHopIP = 0;
-    root->zeroChild = NULL;
-    root->oneChild = NULL;
-    addNodeToTrie("1.2.3.0/16", "10.0.2.17");
+    sock = create_cs3516_socket(myConfig->realNetworkIp);
+    setupOutputQueues();
     while(1) {
         recvNonBlocking(1);
     }
 }
  
-void runHost(int isSendingHost) {
-    if(isSendingHost) {
+void runHost() {
+    if(myConfig->overlayIp == stringIpToByte("1.2.3.1")) {
         cout << "I am a sending host" << endl;
-        sock = create_cs3516_socket(stringIpToByte("10.0.2.15"));
+        sock = create_cs3516_socket(myConfig->realNetworkIp);
         char payload[12] = {"hello world"};
-        sendInitialData(payload, 12, "1.2.3.1");
+        sendInitialData(payload, 12, "4.5.6.1");
     } else {
         cout << "I am a host" << endl;
-        sock = create_cs3516_socket(stringIpToByte("10.0.2.17"));
+        sock = create_cs3516_socket(myConfig->realNetworkIp);
     }
     while(1) {
         recvNonBlocking(0);
@@ -356,34 +481,39 @@ void runHost(int isSendingHost) {
  
 }
  
-// Return 2 for router, 1 for sending host, 0 for regular host, -1 for invalid
-int isRouter(char* argv[]) {
-    if(strcmp(argv[1], "s") == 0){
-        return 1;
-    }
-    if(strcmp(argv[1], "r") == 0){
-        return 2;
-    }
-    if(strcmp(argv[1], "h") == 0){
-        return 0;
-    }
-    return -1;
-}
- 
 int main(int argc, char* argv[]) {
-    if(argc > 1) {
-        int isRouterRes = isRouter(argv);
-        if(isRouterRes == 1 || isRouterRes == 0) {
-            runHost(isRouterRes);
-        } else if(isRouterRes == 2) {
-            runRouter();
-        } else {
-            cerr << "Did not pass in 's', 'r' or 'h'" << endl;
+    if(argc > 2) {
+        int nodeId = atoi(argv[1]);
+        string filename = argv[2];
+        root = (TrieNode*) malloc(sizeof(TrieNode));
+        root->isSet = 0;
+        root->nextHopIP = 0;
+        root->zeroChild = NULL;
+        root->oneChild = NULL;
+        myConfig = (NodeData*) malloc(sizeof(NodeData));
+        outRouter = (NodeData*) malloc(sizeof(NodeData));
+        readConfigFile(filename);
+        map<int, NodeData*>::iterator dataIt = nodeData.find(nodeId);
+        if(dataIt == nodeData.end()) {
+            cerr << "Invalid node id passed in" << endl;
             exit(1);
+        }
+        myConfig->nodeId = dataIt->second->nodeId;
+        myConfig->realNetworkIp = dataIt->second->realNetworkIp;
+        myConfig->overlayIp = dataIt->second->overlayIp;
+        myConfig->isRouter = dataIt->second->isRouter;
+        if(!myConfig->isRouter) {
+            int outRouterId = outRouterData.find(nodeId)->second;
+            outRouter->nodeId = outRouterId;
+            outRouter->realNetworkIp = nodeData.find(outRouterId)->second->realNetworkIp;
+            outRouter->isRouter = true;
+            runHost();
+        } else {
+            runRouter();
         }
     } else {
         // NO PARAMETERS
-        cerr << "Usage ./project3 [s/r/h]" << endl;
+        cerr << "Usage ./project3 NODEID CONFIG_FILENAME" << endl;
         exit(1);
     }
 }
